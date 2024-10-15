5538290ab783ff8c94ec06df2f7c7fff6d71cbad
saif-almalka <saif711almilki@gmail.com>
2024-10-15 12:36:22 +0300

    nothing

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+class calculator
+{
+    
+    static void Main()
+{
+    // عمل الطالب : سيف المالكي
+    Console.WriteLine("Enter the first number : ");
+    double firstnum=Convert.ToInt32 (Console.ReadLine());
+    Console.WriteLine("Enter Tte second number : ");
+    double secondnum=Convert.ToInt32 (Console.ReadLine());
+    Console.WriteLine("Enter an operation ( + , - , / , * )");
+    string oprtstion=Console.ReadLine();
+    double result=0;
+
+    if (oprtstion=="+")
+    {
+        result=firstnum+secondnum;
+    }
+    else if (oprtstion=="-")
+    {
+        result=firstnum-secondnum;
+    }
+    else if (oprtstion=="*")
+    {
+        result=firstnum*secondnum;
+    }
+    else if (oprtstion=="/")
+    {
+       result=firstnum/secondnum;
+    }
+    Console.WriteLine("result = "+result);
+}
+
+}
